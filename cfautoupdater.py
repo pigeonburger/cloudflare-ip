ce80b0de19c89342008d26760a5fda864fb858c0
pygeonburger <70826123+pygeonburger@users.noreply.github.com>
2021-03-07 09:57:03 +1100

    Added Ipify status code check and used JSON format instead.

--- cfautoupdater.py
+++ cfautoupdater.py
@@ -8,7 +8,6 @@ zone_id = config.get('tokens', 'zone_id')
 bearer_token = config.get('tokens', 'bearer_token')
 record_id = config.get('tokens', 'record_id')
 
-
 # The headers we want to use
 headers = {
     "Authorization": f"Bearer {bearer_token}", 
@@ -19,25 +18,39 @@ headers = {
 a_record_url = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers)
 arecordjson = a_record_url.json()
 
-
 # This is the current IP that your chosen A record has been set to on Cloudflare
 current_set_ip = arecordjson['result']['content']
 
+# This gets your current live external IP (whether that is the same as the A record or not)
+currentip = requests.get("https://api.ipify.org?format=json")
 
-# This gets your current real live external IP (whether that is the same as the A record or not)
-currentactualip = requests.get('https://api.ipify.org').text
+# Status code should be 200, otherwise the API is probably down (this can happen quite a bit)
+ipcheck_status = currentip.status_code
 
+# Handling any API errors (otherwise we'd be trying to change the IP to some random HTML)
+while ipcheck_status != 200:
+    time.sleep(10)
+    currentip = requests.get("https://api.ipify.org?format=json")
+    ipcheck_status = currentip.status_code
 
-# This loop checks your live IP every 15 minutes to make sure that it's the same one as set in your DNS record
+currentactualip = currentip.json()['ip']
+
+# This loop checks your live IP every 5 minutes to make sure that it's the same one as set in your DNS record
 while currentactualip == current_set_ip:
-    time.sleep(900) # Wait for 900 seconds (15 minutes)
-    currentactualip = requests.get('https://api.ipify.org').text # Then it checks if your IP is still the same or not.
+    time.sleep(300) # Wait for 300 seconds (5 minutes)
+    currentip = requests.get("https://api.ipify.org?format=json") # Then it checks if your IP is still the same or not.
+    ipcheck_status = currentip.status_code
+
+# Handling any API errors AGAIN
+    while ipcheck_status != 200:
+        time.sleep(10)
+        currentip = requests.get("https://api.ipify.org?format=json")
+        ipcheck_status = currentip.status_code
+
+    currentactualip = currentip.json()['ip']
 
 else: # If your live IP is NOT the same as the A Record's IP
     pass
-
-# Just getting your live IP again (can probs get rid of this as we have it in the loop, but ipify doesn't have rate limits so I can use it as much as I damn want to) 
-currentactualip = requests.get('https://api.ipify.org').text
 
 # The "Payload" is what we want to change in the DNS record JSON (in this case, it's our IP)
 payload = {"content": currentactualip}
