87b96400205ee5a6736ba77dbc415e882ec3ee64
Pigeonburger <pigeonburger@hotmail.com>
2021-03-13 10:29:35 +1100

    Made things clearer

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-# Cloudflare DNS Auto IP Address Updater
+# Cloudflare DNS Auto IP Updater
 
 This is a script I made that will automatically update my A record's IP address whenever my web server's IP changes.
 
 I don't have (nor can I afford) a static ip, meaning that my IP address constantly changes. Whenever it <i>does</i> change (usually while I'm sleeping), it means that my site becomes completely inaccessible. So, I attempted to create a solution, which is this script. It can also send you an email to let you know it's been updated.
 
-Be aware, this only works if your site is on Cloudflare, AND you have direct access to the web server (root permissions are not required). See the requirements below:
+Be aware, this only works if your site is on the Cloudflare CDN. See the requirements below:
 
 <h2>Requirements:</h2>
 
   - Python 3.6 or above
   - Python requests library (install using `pip3 install requests`)
-  - A Cloudflare account
+  - A Cloudflare account with website
   - Cloudflare API Bearer Token
-  - Access to your server's file system
   - Need to know your zone ID
-  - You'll also need to know the ID of the A record you want to change
+  - The ID of the A record you want to change
   
   
 <h2>Optional:</h2>
@@ -28,15 +27,6 @@ On your web server, in any directory, clone this repository:
 
     git clone https://github.com/pygeonburger/cloudflare-ip/
         
-Then, create a file called `cfauth.ini` in that cloned folder and set it up like so:
-
-    [tokens]
-    zone_id=<YOUR_ZONE_ID>
-    bearer_token=<YOUR_BEARER_TOKEN>
-    record_id=<YOUR_DNS_RECORD_ID>
-        
-Replacing `<YOUR_ZONE_ID>` with your actual Cloudflare Zone ID, `<YOUR_BEARER_TOKEN>` with your API bearer token and `<YOUR_DNS_RECORD_ID>` with the ID of the DNS record you want to change.
+Then, open the file `cfauth.ini` in that folder and replace `<YOUR_ZONE_ID>` with your actual Cloudflare Zone ID, `<YOUR_BEARER_TOKEN>` with your API bearer token and `<YOUR_DNS_RECORD_ID>` with the ID of the DNS record you want to change.
 
 Then, if you've done everything, run the `cfautoupdater.py` file!
-
-To keep this script running forever, rather than just stopping once it changes your DNS record's ID, I just use the <a href="https://github.com/immortal/immortal">Immortal</a> package.
--- cfautoupdater.py
+++ cfautoupdater.py
@@ -14,34 +14,21 @@ headers = {
     "content-type": "application/json"
     }
 
-# Getting the initial data of your A Record
-a_record_url = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers)
-arecordjson = a_record_url.json()
+while True:
+    # Getting the initial data of your A Record
+    a_record_url = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers)
+    arecordjson = a_record_url.json()
 
-# This is the current IP that your chosen A record has been set to on Cloudflare
-current_set_ip = arecordjson['result']['content']
+    # This is the current IP that your chosen A record has been set to on Cloudflare
+    current_set_ip = arecordjson['result']['content']
 
-# This gets your current live external IP (whether that is the same as the A record or not)
-currentip = requests.get("https://api.ipify.org?format=json")
-
-# Status code should be 200, otherwise the API is probably down (this can happen quite a bit)
-ipcheck_status = currentip.status_code
-
-# Handling any API errors (otherwise we'd be trying to change the IP to some random HTML)
-while ipcheck_status != 200:
-    time.sleep(10)
+    # This gets your current live external IP (whether that is the same as the A record or not)
     currentip = requests.get("https://api.ipify.org?format=json")
+
+    # Status code should be 200, otherwise the API is probably down (this can happen quite a bit)
     ipcheck_status = currentip.status_code
 
-currentactualip = currentip.json()['ip']
-
-# This loop checks your live IP every 5 minutes to make sure that it's the same one as set in your DNS record
-while currentactualip == current_set_ip:
-    time.sleep(300) # Wait for 300 seconds (5 minutes)
-    currentip = requests.get("https://api.ipify.org?format=json") # Then it checks if your IP is still the same or not.
-    ipcheck_status = currentip.status_code
-
-# Handling any API errors AGAIN
+    # Handling any API errors (otherwise we'd be trying to change the IP to some random HTML)
     while ipcheck_status != 200:
         time.sleep(10)
         currentip = requests.get("https://api.ipify.org?format=json")
@@ -49,35 +36,48 @@ while currentactualip == current_set_ip:
 
     currentactualip = currentip.json()['ip']
 
-else: # If your live IP is NOT the same as the A Record's IP
-    pass
+    # This loop checks your live IP every 5 minutes to make sure that it's the same one as set in your DNS record
+    while currentactualip == current_set_ip:
+        time.sleep(300) # Wait for 300 seconds (5 minutes)
+        currentip = requests.get("https://api.ipify.org?format=json") # Then it checks if your IP is still the same or not.
+        ipcheck_status = currentip.status_code
 
-# The "Payload" is what we want to change in the DNS record JSON (in this case, it's our IP)
-payload = {"content": currentactualip}
+    # Handling any API errors AGAIN
+        while ipcheck_status != 200:
+            time.sleep(10)
+            currentip = requests.get("https://api.ipify.org?format=json")
+            ipcheck_status = currentip.status_code
 
-# Change the IP using a PATCH request
-requests.patch(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers, data=json.dumps(payload))
+        currentactualip = currentip.json()['ip']
+
+    else: # If your live IP is NOT the same as the A Record's IP
+        pass
+
+    # The "Payload" is what we want to change in the DNS record JSON (in this case, it's our IP)
+    payload = {"content": currentactualip}
+
+    # Change the IP using a PATCH request
+    requests.patch(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers, data=json.dumps(payload))
 
 
-# Sends an email to you to let you know everything has been updated.
-# If you don't want this, just delete everything below this comment.
+    # Sends an email to you to let you know everything has been updated.
+    # If you don't want this, just delete everything below this comment.
 
-sender = 'sender@google.com'
-receivers = ['receiver@example.com']
+    sender = 'sender@google.com'
+    receivers = ['receiver@example.com']
 
-message = f"""From: Server <sender@google.com>
-To: Your email <receiver@example.com>
-Subject: DNS IP Updated
+    message = f"""From: Server <sender@google.com>
+    To: Your email <receiver@example.com>
+    Subject: DNS IP Updated
 
-The server's IP has changed from {current_set_ip} to {currentactualip}.
+    The server's IP has changed from {current_set_ip} to {currentactualip}.
 
-The DNS records have been updated.
-"""
+    The DNS records have been updated.
+    """
 
-smtpObj = smtplib.SMTP('smtp.example.com', port=587)
-smtpObj.connect("smtp.example.com", port=587)
-smtpObj.ehlo()
-smtpObj.starttls()
-smtpObj.login("username", "password")
-smtpObj.sendmail(sender, receivers, message)         
-print("Successfully sent email")
+    smtpObj = smtplib.SMTP('smtp.example.com', port=587)
+    smtpObj.connect("smtp.example.com", port=587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login("username", "password")
+    smtpObj.sendmail(sender, receivers, message)
